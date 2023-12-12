49d489c836c3e7a83d3ba46f7c5fab617e0a2c16
maljuraysh <maljuraysh@ncpd.gov.sa>
2023-12-12 15:08:29 +0300

    wip

---
+++ src/MsegatBalance.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BitcodeSa\Msegat;
+
+use Illuminate\Support\Facades\Cache;
+
+class MsegatBalance extends Msegat
+{
+    protected $end_point = "Credits.php";
+
+    public function __construct($username = null)
+    {
+        $this->setApiUrl();
+        $this->setAuthentication();
+        $this->setUsername($username);
+        $this->setClient();
+    }
+
+    public function setRequest()
+    {
+        $this->request = [
+            "userName" => $this->username,
+            "apiKey" => $this->api_key,
+            "msgEncoding" => "UTF8"
+        ];
+    }
+
+    public function balance()
+    {
+        $this->setRequest();
+        $this->response = $this->client->post("/".$this->end_point, $this->request);
+        logger($this->response);
+        return $this->response;
+    }
+}
