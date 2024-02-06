cce868e000953edca9da8aacb1ddd626e1ede1a4
maljuraysh <maljuraysh@ncpd.gov.sa>
2024-02-06 11:21:49 +0300

    bug fix

--- src/Models/Message.php
+++ src/Models/Message.php
@@ -7,12 +7,21 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Message extends Model
 {
+    protected $fillable = [
+        'phone',
+        'message',
+        'response_code',
+        'response_message',
+        'messageable_type',
+        'messageable_id',
+    ];
+
     public function getTable()
     {
         return config('msegat.model.table_name', parent::getTable());
     }
 
-    public function messageable():MorphTo
+    public function messageable(): MorphTo
     {
         return $this->morphTo();
     }
